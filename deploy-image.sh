ef1c65429d6f33877301bfbe25b40ca627170bff
FinnieRyan <ryanfinnieston@gmail.com>
2023-12-18 12:25:59 +0000

    refactor

---
+++ deploy-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Update the deployment image
+kubectl set image deployment/cw2app cw2app=ryanfinnie/cw2-app:latest --kubeconfig=/home/ubuntu/.kube/config
+
+# Check if successful
+kubectl rollout status deployment/cw2app --kubeconfig=/home/ubuntu/.kube/config
